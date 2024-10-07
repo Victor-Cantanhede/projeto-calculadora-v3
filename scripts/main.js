e818972929b8459ab0bc3e661925264d4bbd50e4
Victor-Cantanhede <victorcantanhedblk@gmail.com>
2024-10-07 11:32:50 -0300

    testes funcionalidades

--- scripts/main.js
+++ scripts/main.js
@@ -9,20 +9,36 @@ let resultado = '';
 
 // FUNÇÃO PARA INSERIR NA TELA
 function inputscreen001(p) {
-    inputScreen001 += p;
-    screen001.innerHTML = inputScreen001;
+    if (resultado != '' && (p == '/' || p == '*' || p == '-' || p == '+')) {
+        inputScreen001 += p;
+        screen001.innerHTML = inputScreen001;
 
-    inputScreen002 += p;
-    screen002.innerHTML = inputScreen002;
+    } else if (resultado != '' && inputScreen001 == resultado) {
+        inputScreen001 += p;
+        screen001.innerHTML = inputScreen001;
+    
+        resultado = eval(inputScreen001);
+        inputScreen002 = resultado;
+        screen002.innerHTML = inputScreen002;
+
+    } else {
+        inputScreen001 += p;
+        screen001.innerHTML = inputScreen001;
+    
+        resultado = eval(inputScreen001);
+        inputScreen002 = resultado;
+        screen002.innerHTML = inputScreen002;
+    }
 }
 
 // FUNÇÃO PARA CALCULAR RESULTADO
 function calcular() {
-    resultado = eval(inputScreen002);
-    screen001.innerHTML = resultado;
-    inputScreen001 = '';
-    inputScreen002 += '=';
+    inputScreen002 = `${inputScreen001}=`;
     screen002.innerHTML = inputScreen002;
+
+    inputScreen001 = resultado;
+    screen001.innerHTML = inputScreen001;
+
     inputScreen002 = '';
 }
 
